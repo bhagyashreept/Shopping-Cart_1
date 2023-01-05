aca60fd8da0452bfac3167afa03f408a764ab430
bhagyashreept <55779449+bhagyashreept@users.noreply.github.com>
2023-01-05 23:48:43 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Shopping-Cart_1
+# Shopping-Cart
 Created with CodeSandbox
---
+++ src/components/Cart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cart = (state, dispatch) => {
+  return <div>Cart</div>;
+};
+
+export default Cart;
